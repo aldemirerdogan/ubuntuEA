1a44b82f213d12a77c625d661558caccbbcbaf72
Erdogan Aldemir <aldemir.erdogan@gmail.com>
2023-12-29 22:29:47 +0300

    Update hello.py
    denemememme

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ def factorial(n):  // function...fgdgdgdg
 		return 1
 	else:
 		return n*factorial(n-1)
-		
+		fgghffh
 number = 5
 result = factorial(number)
 print(f"the factorial of {number} is {result}.")
